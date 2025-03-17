8725ce56a7ed31d9c6245c57e55f22cdd8c3ddb7
rafaelreisramos <rrramos@gmail.com>
2025-03-17 16:09:07 -0300

    chore: validate friend input

--- challenge-amigo-secreto/app.js
+++ challenge-amigo-secreto/app.js
@@ -2,8 +2,18 @@ let amigos = [];
 
 function adicionarAmigo() {
   const input = document.getElementById('amigo');
-  const amigo = input.value;
+  const amigo = input.value.trim().toLowerCase();
   input.value = '';
+
+  if (!amigo) {
+    alert('Por favor, insira um nome.');
+    return;
+  }
+
+  if (amigos.includes(amigo)) {
+    alert('Um amigo com este nome já foi inserido.');
+    return;
+  }
 
   amigos.push(amigo);
   console.log(amigos);
